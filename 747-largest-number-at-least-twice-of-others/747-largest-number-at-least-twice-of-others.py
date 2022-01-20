382d740e1c2cd55ae9e47e56f64648f6dfc4c5d6
Andy taipe <55109873+taipe1@users.noreply.github.com>
2022-01-19 23:51:58 -0500

    Time: 62 ms (14.29%), Space: 14.3 MB (44.53%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
@@ -0,0 +1,9 @@
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        maximum = max(nums)
+        for i in range(len(nums)):
+            if nums[i] == maximum:
+                maxindex = i
+            if nums[i] != maximum and maximum < 2*(nums[i]):
+                return -1
+        return maxindex
