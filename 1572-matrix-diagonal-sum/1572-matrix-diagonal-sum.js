f30f2dd52296e3c936be17f8d5769bc737026dc4
Andy taipe <55109873+taipe1@users.noreply.github.com>
2022-02-04 12:03:51 -0500

    Time: 96 ms (43.56%), Space: 42.9 MB (13.80%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -4,8 +4,8 @@
  */
 var diagonalSum = function(mat) {
     let suma = 0
-    for(let i=0 ; i < mat.length ; i++){
-        for(let j=0 ; j < mat[i].length ; j++){
+    for(let i = 0 ; i < mat.length ; i++){
+        for(let j = 0 ; j < mat[i].length ; j++){
             if( (i == j) || ( (i+j) == mat[i].length - 1 ) ){
                 suma = suma + mat[i][j]
             }
