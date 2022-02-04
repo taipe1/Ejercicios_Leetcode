249b1230db258e887731f4546cd5c7b7f7d7ffd0
Andy taipe <55109873+taipe1@users.noreply.github.com>
2022-02-03 20:50:12 -0500

    Time: 79 ms (58.91%), Space: 16.1 MB (27.61%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        aux = {}
+        for i in nums:
+            if(i not in aux):
+                aux[i]=1
+            else:
+                aux[i]=aux[i]+1
+                
+        for a,b in aux.items():
+            if( b==1):
+                return a
+            
+        
