d5a09e8e3297c6e9e73cd28c1024127191b366ef
Andy taipe <55109873+taipe1@users.noreply.github.com>
2022-02-11 23:57:14 -0500

    Time: 93 ms (46.96%), Space: 13.8 MB (76.13%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
@@ -1,5 +1,5 @@
 class Solution:
     def sortEvenOdd(self, A: List[int]) -> List[int]:
-        A[::2], A[1::2] = sorted(A[::2]), sorted(A[1::2])[::-1]
+        A[::2], A[1::2] = sorted(A[::2]), sorted(A[1::2], reverse = True)
         return A
         
